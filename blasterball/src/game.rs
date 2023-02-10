aa8aaa5e0679edb147113184a7a2194a2e47ac8f
demilade <sonugademilade8703@gmail.com>
2023-02-10 20:19:17 +0100

    added code for the second interrupt descriptor table post

---
+++ blasterball/src/game.rs
@@ -0,0 +1,56 @@
+use crate::uefi::{Screen, NO_OF_PIXELS_IN_A_ROW, NO_OF_PIXELS_IN_A_COLUMN};
+use crate::bitmap::{draw_bitmap, Bitmap};
+
+// The number of rows of blocks that are going to be drawn on screen
+const NO_OF_BLOCK_ROWS: usize = 4;
+
+pub fn blasterball(screen: &mut Screen) -> ! {
+    // Loading the blocks into an array
+    let blue_block_bytes = include_bytes!("./assets/blue_block.bmp");
+    let blue_block = Bitmap::new(blue_block_bytes);
+    let cyan_block_bytes = include_bytes!("./assets/cyan_block.bmp");
+    let cyan_block = Bitmap::new(cyan_block_bytes);
+    let green_block_bytes = include_bytes!("./assets/green_block.bmp");
+    let green_block = Bitmap::new(green_block_bytes);
+    let pink_block_bytes = include_bytes!("./assets/pink_block.bmp");
+    let pink_block = Bitmap::new(pink_block_bytes);
+    let yellow_block_bytes = include_bytes!("./assets/yellow_block.bmp");
+    let yellow_block = Bitmap::new(yellow_block_bytes);
+    let paddle_bytes = include_bytes!("./assets/paddle.bmp");
+    let paddle = Bitmap::new(paddle_bytes);
+    let ball_bytes = include_bytes!("./assets/ball.bmp");
+    let ball = Bitmap::new(ball_bytes);
+    let blocks = [blue_block, cyan_block, green_block, pink_block, yellow_block];
+
+    // The initial block position is at the top left corner
+    // of the screen
+    let mut block_position = (0, 0); // (row, column)
+
+    // Cycle through the blocks until 4 rows have been filled
+    for (i, block) in blocks.iter().cycle().enumerate() {
+        let no_of_blocks_in_a_row = NO_OF_PIXELS_IN_A_ROW / block.width();
+        if i >= no_of_blocks_in_a_row * NO_OF_BLOCK_ROWS {
+            break;
+        }
+        draw_bitmap(screen, block, block_position);
+        block_position.1 += block.width();
+        if block_position.1 >= NO_OF_PIXELS_IN_A_ROW {
+            block_position.0 += block.height();
+            block_position.1 = 0;
+        }
+    }
+
+    let initial_paddle_position = (
+        NO_OF_PIXELS_IN_A_COLUMN - paddle.height() - 10,
+        NO_OF_PIXELS_IN_A_ROW / 2 - 45
+    );
+    draw_bitmap(screen, &paddle, initial_paddle_position);
+
+    let initial_ball_position = (
+        initial_paddle_position.0 - ball.height() - 5,
+        initial_paddle_position.1 + paddle.width() / 2 - 10
+    );
+    draw_bitmap(screen, &ball, initial_ball_position);
+    
+    loop {}
+}
---
+++ blasterball/src/interrupts.rs
@@ -0,0 +1,105 @@
+use core::arch::asm;
+use crate::gdt::SegmentSelector;
+
+// Tells the processor to stop responding to interrupts
+pub fn disable_interrupts() {
+    unsafe {
+        asm!("cli");
+    }
+}
+
+// Tells the processor to start responding to interrupts
+pub fn enable_interrupts() {
+    unsafe {
+        asm!("sti");
+    }
+}
+
+// An entry in the IDT
+#[repr(C)]
+pub struct Entry {
+    // The lower 16 bits of the handler's interrupt service routine
+    handler_ptr_low: u16,
+    // The segment selector of the code segment
+    segment_selector: SegmentSelector,
+    // The IDT entry options
+    options: Options,
+    // The next 16 bits of the interrupt service routine
+    handler_ptr_middle: u16,
+    // The upper 32 bits of the interrupt service routine
+    handler_ptr_upper: u32,
+    reserved: u32
+}
+
+impl Entry {
+    // Creates an empty entry
+    fn empty() -> Self {
+        Self {
+            handler_ptr_low: 0,
+            segment_selector: SegmentSelector(0),
+            options: Options(0),
+            handler_ptr_middle: 0,
+            handler_ptr_upper: 0,
+            reserved: 0
+        }
+    }
+}
+
+// Options in an IDT entry
+#[repr(transparent)]
+struct Options(u16);
+
+// The Interrupt Descriptor Table
+#[repr(C, align(8))]
+pub struct IDT {
+    pub divide_by_zero: Entry,
+    pub debug: Entry,
+    pub nmi_interrupt: Entry,
+    pub breakpoint: Entry,
+    pub overflow: Entry,
+    pub bound_range_exceeded: Entry,
+    pub invalid_opcode: Entry,
+    pub device_not_available: Entry,
+    pub double_fault: Entry,
+    pub coprocessor_segment_overrun: Entry,
+    pub invalid_tss: Entry,
+    pub segment_not_present: Entry,
+    pub stack_segment_fault: Entry,
+    pub general_protection: Entry,
+    pub page_fault: Entry,
+    reserved1: Entry,
+    pub floating_point_error: Entry,
+    pub alignment_check: Entry,
+    pub machine_check: Entry,
+    pub simd_floating_point_exception: Entry,
+    pub virtualization_exception: Entry,
+    pub control_protection_exception: Entry,
+    reserved2: [Entry; 10],
+    pub interrupts: [Entry; 256 - 32]
+}
+
+// The values pushed on the stack when an interrupt service routine
+// is called by the processor
+#[repr(C)]
+struct InterruptStackFrame {
+    // The address of the instruction that was executing
+    // before the processor switched to the service routine
+    original_instruction_ptr: u64,
+    // The code segment selector that was being used before
+    // the control switch to the service routine
+    // It is padded to become 64 bits
+    original_code_segment: u64,
+    // The contents of the FLAGS register at the time of
+    // the control switch to the service routine
+    flags: u64,
+    // Address of the stack at the time of the switch
+    original_stack_ptr: u64,
+    // The stack segment selector at the time of the switch
+    original_stack_segment: u64
+}
+
+type ServiceRoutine = extern "x86-interrupt" fn(InterruptStackFrame);
+
+type ServiceRoutineWithErrCode = extern "x86-interrupt" fn(InterruptStackFrame, u64);
+
+type ServiceRoutineWithNoReturn = extern "x86-interrupt" fn(InterruptStackFrame, u64) -> !;
