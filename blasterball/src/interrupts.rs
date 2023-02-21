c3843f45b3f2ced84ab7e2fce4e62d24bcdb6fe1
demilade <sonugademilade8703@gmail.com>
2023-02-21 11:04:50 +0100

    added the code for the some important exceptions post

---
+++ blasterball/src/interrupts.rs
@@ -0,0 +1,284 @@
+use core::arch::asm;
+use crate::gdt::SegmentSelector;
+use core::marker::PhantomData;
+use core::ops::{BitAnd, Shr};
+use crate::gdt::DescriptorTablePointer;
+
+// Tells the processor to stop responding to interrupts
+pub fn disable_interrupts() {
+    unsafe {
+        asm!("cli");
+    }
+}
+
+// Tells the processor to start responding to interrupts
+pub fn enable_interrupts() {
+    unsafe {
+        asm!("sti");
+    }
+}
+
+// An entry in the IDT
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct Entry<T> {
+    // The lower 16 bits of the handler's interrupt service routine
+    handler_ptr_low: u16,
+    // The segment selector of the code segment
+    segment_selector: SegmentSelector,
+    // The IDT entry options
+    options: Options,
+    // The next 16 bits of the interrupt service routine
+    handler_ptr_middle: u16,
+    // The upper 32 bits of the interrupt service routine
+    handler_ptr_upper: u32,
+    reserved: u32,
+    // Dummy field to resolve problem of no field making
+    // use of generic type
+    phantom: PhantomData<T>
+}
+
+impl<T: BitAnd<u16, Output=u16> 
+    + Shr<u64, Output=u16> 
+    + Shr<u32, Output=u32> 
+    + Copy> Entry<T> {
+    // Creates an empty entry
+    fn empty() -> Self {
+        Self {
+            handler_ptr_low: 0,
+            segment_selector: SegmentSelector(0),
+            options: Options(0),
+            handler_ptr_middle: 0,
+            handler_ptr_upper: 0,
+            reserved: 0,
+            phantom: PhantomData
+        }
+    }
+
+    // Creates a new entry for exceptions
+    pub fn exception(handler_ptr: T, segment_selector: SegmentSelector) -> Self {
+        Self::new(handler_ptr, segment_selector, Options::exception())
+    }
+
+    // Creates a new entry for interrupts
+    pub fn interrupt(handler_ptr: T, segment_selector: SegmentSelector) -> Self {
+        Self::new(handler_ptr, segment_selector, Options::interrupt())
+    }
+
+    // Creates a new entry with the specified options
+    fn new(handler_ptr: T, segment_selector: SegmentSelector, options: Options) -> Self {
+        let mut entry = Self::empty();
+        // Clearing out all bits except the lower 16 bits of the routine
+        // function pointer and setting it as the entry's handler pointer lower bits
+        entry.handler_ptr_low = handler_ptr & 0xffffu16;
+        // Shifting out the lower 16 bits leaving the next 16 bits in its
+        // place and setting it as the entry's handler pointer middle bits
+        entry.handler_ptr_middle = (handler_ptr >> 16u64) & 0xffffu16;
+        // Clearing out the lower 32 bits and retaining only the upper 32
+        // Then setting it as the entry's handler pointer higher bits
+        entry.handler_ptr_upper = (handler_ptr >> 32u32) & 0xffffffffu32;
+
+        // Setting the segment selector of the entry
+        entry.segment_selector = segment_selector;
+
+        entry.options = options;
+
+        entry
+    }
+}
+
+// Options in an IDT entry
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+struct Options(u16);
+
+impl Options {
+    // Creates the options for an exception entry in the IDT
+    fn exception() -> Self {
+        Self(0b1000111100000000)
+    }
+
+    // Creates the options for a maskable interrupt entry in the IDT
+    fn interrupt() -> Self {
+        Self(0b1000111000000000)
+    }
+}
+
+// The Interrupt Descriptor Table
+#[repr(C, align(8))]
+pub struct IDT {
+    pub divide_by_zero: Entry<ServiceRoutine>,
+    pub debug: Entry<ServiceRoutine>,
+    pub nmi_interrupt: Entry<ServiceRoutine>,
+    pub breakpoint: Entry<ServiceRoutine>,
+    pub overflow: Entry<ServiceRoutine>,
+    pub bound_range_exceeded: Entry<ServiceRoutine>,
+    pub invalid_opcode: Entry<ServiceRoutine>,
+    pub device_not_available: Entry<ServiceRoutine>,
+    pub double_fault: Entry<ServiceRoutineWithNoReturn>,
+    pub coprocessor_segment_overrun: Entry<ServiceRoutine>,
+    pub invalid_tss: Entry<ServiceRoutineWithErrCode>,
+    pub segment_not_present: Entry<ServiceRoutineWithErrCode>,
+    pub stack_segment_fault: Entry<ServiceRoutineWithErrCode>,
+    pub general_protection: Entry<ServiceRoutineWithErrCode>,
+    pub page_fault: Entry<ServiceRoutineWithErrCode>,
+    reserved1: Entry<ServiceRoutine>,
+    pub floating_point_error: Entry<ServiceRoutine>,
+    pub alignment_check: Entry<ServiceRoutineWithErrCode>,
+    pub machine_check: Entry<ServiceRoutineWithNoReturn>,
+    pub simd_floating_point_exception: Entry<ServiceRoutine>,
+    pub virtualization_exception: Entry<ServiceRoutine>,
+    pub control_protection_exception: Entry<ServiceRoutineWithErrCode>,
+    reserved2: [Entry<ServiceRoutine>; 10],
+    pub interrupts: [Entry<ServiceRoutine>; 256 - 32]
+}
+
+impl IDT {
+    pub fn new() -> Self {
+        Self {
+            divide_by_zero: Entry::empty(),
+            debug: Entry::empty(),
+            nmi_interrupt: Entry::empty(),
+            breakpoint: Entry::empty(),
+            overflow: Entry::empty(),
+            bound_range_exceeded: Entry::empty(),
+            invalid_opcode: Entry::empty(),
+            device_not_available: Entry::empty(),
+            double_fault: Entry::empty(),
+            coprocessor_segment_overrun: Entry::empty(),
+            invalid_tss: Entry::empty(),
+            segment_not_present: Entry::empty(),
+            stack_segment_fault: Entry::empty(),
+            general_protection: Entry::empty(),
+            page_fault: Entry::empty(),
+            reserved1: Entry::empty(),
+            floating_point_error: Entry::empty(),
+            alignment_check: Entry::empty(),
+            machine_check: Entry::empty(),
+            simd_floating_point_exception: Entry::empty(),
+            virtualization_exception: Entry::empty(),
+            control_protection_exception: Entry::empty(),
+            reserved2: [Entry::empty(); 10],
+            interrupts: [Entry::empty(); 256 - 32]
+        }
+    }
+
+    // Creates a descriptor table pointer that tells to tell
+    // the processor where the IDT is located
+    pub fn as_pointer(&'static self) -> DescriptorTablePointer {
+        DescriptorTablePointer {
+            limit: (core::mem::size_of::<Self>() - 1) as u16,
+            base: self as *const _ as *const u8
+        }
+    }
+}
+
+pub fn load_idt(pointer: &DescriptorTablePointer) {
+    unsafe {
+        asm!("lidt [{}]", in(reg) pointer);
+    }
+}
+
+// The values pushed on the stack when an interrupt service routine
+// is called by the processor
+#[repr(C)]
+pub struct InterruptStackFrame {
+    // The address of the instruction that was executing
+    // before the processor switched to the service routine
+    original_instruction_ptr: u64,
+    // The code segment selector that was being used before
+    // the control switch to the service routine
+    // It is padded to become 64 bits
+    original_code_segment: u64,
+    // The contents of the FLAGS register at the time of
+    // the control switch to the service routine
+    flags: u64,
+    // Address of the stack at the time of the switch
+    original_stack_ptr: u64,
+    // The stack segment selector at the time of the switch
+    original_stack_segment: u64
+}
+
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct ServiceRoutine(pub extern "x86-interrupt" fn(InterruptStackFrame));
+
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct ServiceRoutineWithErrCode(pub extern "x86-interrupt" fn(InterruptStackFrame, u64));
+
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct ServiceRoutineWithNoReturn(pub extern "x86-interrupt" fn(InterruptStackFrame, u64) -> !);
+
+impl BitAnd<u16> for ServiceRoutine {
+    type Output = u16;
+
+    fn bitand(self, rhs: u16) -> u16 {
+        (self.0 as u16) & rhs
+    }
+}
+
+impl BitAnd<u16> for ServiceRoutineWithErrCode {
+    type Output = u16;
+
+    fn bitand(self, rhs: u16) -> u16 {
+        (self.0 as u16) & rhs
+    }
+}
+
+impl BitAnd<u16> for ServiceRoutineWithNoReturn {
+    type Output = u16;
+
+    fn bitand(self, rhs: u16) -> u16 {
+        (self.0 as u16) & rhs
+    }
+}
+
+impl Shr<u64> for ServiceRoutine {
+    type Output = u16;
+
+    fn shr(self, rhs: u64) -> u16 {
+        (self.0 as u64 >> rhs) as u16
+    }
+}
+
+impl Shr<u64> for ServiceRoutineWithErrCode {
+    type Output = u16;
+
+    fn shr(self, rhs: u64) -> u16 {
+        (self.0 as u64 >> rhs) as u16
+    }
+}
+
+impl Shr<u64> for ServiceRoutineWithNoReturn {
+    type Output = u16;
+
+    fn shr(self, rhs: u64) -> u16 {
+        (self.0 as u64 >> rhs) as u16
+    }
+}
+
+impl Shr<u32> for ServiceRoutine {
+    type Output = u32;
+
+    fn shr(self, rhs: u32) -> u32 {
+        (self.0 as u64 >> rhs as u64) as u32
+    }
+}
+
+impl Shr<u32> for ServiceRoutineWithErrCode {
+    type Output = u32;
+
+    fn shr(self, rhs: u32) -> u32 {
+        (self.0 as u64 >> rhs as u64) as u32
+    }
+}
+
+impl Shr<u32> for ServiceRoutineWithNoReturn {
+    type Output = u32;
+
+    fn shr(self, rhs: u32) -> u32 {
+        (self.0 as u64 >> rhs as u64) as u32
+    }
+}
---
+++ blasterball/src/tss.rs
@@ -0,0 +1,42 @@
+use core::arch::asm;
+use crate::gdt::SegmentSelector;
+
+// The Task State Segment
+#[repr(C, packed)]
+pub struct TSS {
+    reserved1: u32,
+    // Stack pointers for different privilege levels
+    privilege_stack_table: [*mut u8; 3],
+    reserved2: u64,
+    // Stack pointers for switching stacks when an interrupt handler wants to
+    interrupt_stack_table: [*mut u8; 7],
+    reserved3: u64,
+    reserved4: u16,
+    // Offset from the TSS start address to the IO permission bit map
+    io_map_base_addr: u16
+}
+
+impl TSS {
+    // Creates a new TSS with all the stack pointers set to null pointers
+    // and no IO permission bit map
+    pub fn new() -> Self {
+        Self {
+            privilege_stack_table: [core::ptr::null_mut(); 3],
+            interrupt_stack_table: [core::ptr::null_mut(); 7],
+            // Setting this field to the size of the TSS indicates that
+            // there is no IO permission bit map
+            io_map_base_addr: core::mem::size_of::<TSS>() as u16,
+            // Always better to leave the reserved fields as 0s
+            reserved1: 0,
+            reserved2: 0,
+            reserved3: 0,
+            reserved4: 0
+        }
+    }
+}
+
+pub fn load_tss(sel: SegmentSelector) {
+    unsafe {
+        asm!("ltr {}", in(reg) sel.0);
+    }
+}
